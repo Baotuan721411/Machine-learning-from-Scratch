e0e3b49b5b11a45dc83f633bb6bc11c4681e2e01
bao tuan <baotuan.com>
2025-10-04 20:24:53 +0700

    modified

--- KNN.py
+++ KNN.py
@@ -1,31 +1,35 @@
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
 import numpy as np
 from collections import Counter
-iris_data = datasets.load_iris()
-X, y = iris_data.data, iris_data.target
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state= 42)
-def cal_Euclid(x1, x2):
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+data = datasets.load_iris()
+X = data.data
+y = data.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state= 42)
+print(X.shape)
+plt.scatter(X_train[:, 0], X_train[:, 1], c = y_train)
+plt.show()
+def cal_euclid(x1, x2):
     return np.sqrt(np.sum((x1 - x2) ** 2))
 class KNN:
-    def __init__(self, k):
-        self.k = k
+    def __init__(self, K = 3):
+        self.k = K
     def fit(self, X, y):
         self.X = X
         self.y = y
     def predict(self, X):
-        labels = [self._predict(x) for x in X]
-        return labels
+        predictions = [self._predict(x) for x in  X]
+        return predictions
     def _predict(self, x):
-        distances = [cal_Euclid(x, x_i) for x_i in self.X]
+        distances = [cal_euclid(x_i, x) for x_i in self.X]
         k_idx = np.argsort(distances)[:self.k]
         k_label = [self.y[i] for i in k_idx]
-        label = Counter(k_label).most_common(1)
-        return label[0][0]
+        most_common = Counter(k_label).most_common(1)
+        return most_common[0][0]
 
-model = KNN(3)
+model = KNN()
 model.fit(X_train, y_train)
-predicts = model.predict(X_test)
-acc = np.sum(predicts == y_test) / len(y_test) * 100
+predictions = model.predict(X_test)
+acc = np.sum(predictions == y_test) / len(y_test) * 100
 print(acc)
