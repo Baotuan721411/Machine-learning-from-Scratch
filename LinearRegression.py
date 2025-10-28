cc113afca0257c87bc4b78db2b904ab3d8a25fee
Baotuan721411 <truongdoanbaotuan2020@gmail.com>
2025-10-28 21:44:39 +0700

    new change

--- LinearRegression.py
+++ LinearRegression.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
-np.random.seed()
+np.random.seed(42)
 X, y = datasets.make_regression(n_samples= 500, n_features= 1, noise = 20, random_state= 42)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state= 42)
 class LinearRegressionGD:
