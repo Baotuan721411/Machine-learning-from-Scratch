16661f6437a544eaa758ee60b94f5308d0bea39d
Baotuan721411 <truongdoanbaotuan2020@gmail.com>
2025-11-19 22:56:03 +0700

    new change

--- K_means_clustering.py
+++ K_means_clustering.py
@@ -1,65 +1,60 @@
 import numpy as np
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-np.random.seed(42)
-X, y = datasets.make_blobs(n_samples= 5000, n_features = 2, centers = 3, random_state= 42)
-
+X, y = datasets.make_blobs(n_samples= 5000, n_features= 2, centers= 3, random_state= 42)
 plt.scatter(X[:, 0], X[:, 1], c = y)
 def cal_Euclid(x1, x2):
     return np.sqrt(np.sum((x1 - x2) ** 2))
 
 class K_means_clustering:
-    def __init__(self, K = 3, n_iters = 10000):
+    def __init__(self, n_iter = 1000, K = 3):
         self.K = K
-        self.n_iters = n_iters
-        self.clusters = [[] for _ in range(self.K)]
-        self.centrals = []
+        self.n_iter = n_iter
+        self.centers = []
+        self.clusters = [[] for i in range(K)]
+        self.X = None
+        self.n_samples, self.n_features = None, None
     def predict(self, X):
         self.X = X
-        self.examples, self.features = X.shape
-        # tạo central ngẫu nhiên
-        random_idx = np.random.choice(self.examples, size= self.K, replace = False)
-        self.centrals = [self.X[idx] for idx in random_idx]
-        for _ in range(self.n_iters):
-            # phân cụm cho central
-            self.clusters = self._make_clusters(self.centrals)
-            # lưu central cũ
-            old_central = self.centrals
-            # cập nhật central mới
-            self.centrals = self._update_central(self.clusters)
-            # kiểm tra hội tụ
-            if self.converge(old_central, self.centrals):
+        self.n_samples, self.n_features = X.shape
+        random_idx = np.random.choice(self.n_samples, size = self.K, replace = False)
+        self.centers = [self.X[idx] for idx in random_idx]
+        for i in range(self.n_iter):
+            self.clusters = self.make_cluster(self.centers)
+            old_centers = self.centers
+            self.centers = self.make_new_centers(self.clusters)
+            if self.converge(old_centers, self.centers):
                 break
         return self.get_cluster_label(self.clusters)
-
     def get_cluster_label(self, clusters):
-        label = np.empty(self.examples)
-        for cluster_idx, cluster in enumerate(clusters):
-            for sample_idx in  cluster:
-                label[sample_idx] = cluster_idx
+        label = np.empty(self.n_samples)
+        for center_idx, cluster in enumerate(clusters):
+            for cluster_idx in cluster:
+                label[cluster_idx] = center_idx
         return label
-    def converge(self, old, new):
-        distances = [cal_Euclid(old[i], new[i]) for i in range(self.K)]
-        return sum(distances) == 0
-    def _make_clusters(self, centrals):
-        cluster = [[] for _ in range(self.K)]
-        for idx, sample in enumerate(self.X):
-            closet_central_idx = self._find_closet_central_idx(sample, centrals)
-            cluster[closet_central_idx].append(idx)
-        return cluster
-
-    def _find_closet_central_idx(self, sample, centrals):
-        distance = [cal_Euclid(sample, central) for central in centrals]
-        return np.argmin(distance)
-    def _update_central(self, clusters):
-        central = np.zeros((self.K, self.features))
+    def make_cluster(self, centers):
+        clusters = [[] for i in range(self.K)]
+        for idx, point in enumerate(self.X):
+            distance = []
+            for i in range(self.K):
+                dis_to_center_ith = cal_Euclid(point, centers[i])
+                distance.append(dis_to_center_ith)
+            idx_min = np.argmin(distance)
+            clusters[idx_min].append(idx)
+        return clusters
+    def make_new_centers(self, clusters):
+        centers = np.zeros((self.K, self.n_features))
         for idx, cluster in enumerate(clusters):
-            central[idx] = np.mean(self.X[cluster], axis = 0)
-        return central
+            centers[idx] = np.mean(self.X[cluster], axis = 0)
+        return centers
+    def converge(self, old, new):
+        distance = [cal_Euclid(old[idx], new[idx]) for idx in range(self.K)]
+        return np.sum(distance) == 0
 
-model = K_means_clustering(3)
+model = K_means_clustering()
 prediction = model.predict(X)
-for i in range(model.K):
-    plt.scatter(model.centrals[i][0], model.centrals[i][1], marker= 'x')
+for x, y in model.centers:
+    plt.scatter(x, y, marker= 'x', c = 'k')
 plt.show()
--- LinearRegression.py
+++ LinearRegression.py
@@ -3,9 +3,14 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 from Gradient_Descent import GradientDescent
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 np.random.seed(42)
 X, y = datasets.make_regression(n_samples= 500, n_features= 1, noise = 20, random_state= 42)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state= 42)
+modelLR = LinearRegression()
+modelLR.fit(X_train, y_train)
+predict = modelLR.predict(X_test)
 GD = GradientDescent(X_train, y_train)
 class LinearRegression:
     def __init__(self, n_iter, lr):
@@ -14,7 +19,7 @@ class LinearRegression:
         self.w = None
         self.b = None
     def fit(self, X, y):
-        self.w, self.b = GD.adam(0.9, 0.999, 1e-8, 1, 1)
+        self.w, self.b = GD.adam(0.9, 0.99, 1e-8, 2, 2)
     def predict(self, X):
         predictions = np.dot(X, self.w) + self.b
         return predictions
@@ -36,11 +41,18 @@ class LinearRegressionNE:
 
 model = LinearRegression(1000, 0.1)
 model.fit(X_train, y_train)
-predictions = model.predict(X_test)
-plt.plot(X_test, predictions, c= 'r', linewidth= 10)
+predictionGD = model.predict(X_test)
+plt.plot(X_test, predictionGD, c= 'r', linewidth= 10)
 modelNE = LinearRegressionNE()
 modelNE.fit(X_train, y_train)
 predictionsNE = modelNE.predict(X_test)
 plt.scatter(X_test, y_test)
 plt.plot(X_test, predictionsNE, c = 'b')
+plt.plot(X_test, predict, c = 'g')
 plt.show()
+mseSK = mean_squared_error(y_test, predict)
+mseNE = mean_squared_error(y_test, predictionsNE)
+mseGD = mean_squared_error(y_test, predictionGD)
+print('Mean square calculated by sklearn: ', mseSK)
+print('Mean square calculated by NE: ', mseNE)
+print('Mean square calculated by GD: ', mseGD)
